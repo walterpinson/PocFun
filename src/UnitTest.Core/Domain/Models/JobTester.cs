a2aa10df247801122561c8db6fe6e78e205af430
Walter Pinson <walter@walterpinson.com>
2013-06-20 11:31:45 -0400

    Implement tests for the the two current behaviors.

--- src/UnitTest.Core/Domain/Models/JobTester.cs
+++ src/UnitTest.Core/Domain/Models/JobTester.cs
@@ -1,21 +1,46 @@
 ﻿using NUnit.Framework;
 using Core.Domain.Models;
+using NSubstitute;
 
 namespace UnitTest.Core.Domain.Models
 {
     [TestFixture]
     public class JobTester
     {
+        private Job _subjectUnderTest;
+
+        [TearDown]
+        private void TearDown()
+        {
+            _subjectUnderTest = null;
+        }
+
         [Test]
         public void CanConstructJobWithDefaultConstructor() {
             // ARRANGE
 
             // ACT
-            var job = new Job();
+            _subjectUnderTest = new Job();
 
             // ASSERT
-            Assert.That(job, Is.Not.Null);
-            Assert.That(job.IsFilled, Is.False);
+            Assert.That(_subjectUnderTest, Is.Not.Null);
+            Assert.That(_subjectUnderTest.IsFilled, Is.False);
+        }
+
+        [Test]
+        public void CanFillTheJob()
+        {
+            // ARRANGE
+            var candidate = Substitute.For<JobApplicant>();
+            candidate.Id = System.Guid.NewGuid();
+            _subjectUnderTest = new Job();
+
+            // ACT
+            _subjectUnderTest.Fill(candidate);
+
+            // ASSERT
+            Assert.That(_subjectUnderTest.IsFilled, Is.True);
+            Assert.That(_subjectUnderTest.PersonHired, Is.EqualTo(candidate));
         }
     }
 }
