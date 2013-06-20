078323be73efcacb01e56c96c938e6b0706385ab
Walter Pinson <walter@walterpinson.com>
2013-06-20 12:19:21 -0400

    Implement test for valid database configuration using EF db initialize.

--- src/UnitTest.Infrastructure.Data.SqlAzure/PocFunDbContextTester.cs
+++ src/UnitTest.Infrastructure.Data.SqlAzure/PocFunDbContextTester.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using Infrastructure.Data.SqlAzure;
 
 namespace UnitTest.Infrastructure.Data.SqlAzure
 {
-    class PocFunDbContextTester
+    [TestFixture]
+    public class PocFunDbContextTester
     {
+        private PocFunDbContext _context;
+
+        [TestFixtureSetUp]
+        private void TestFixtureSetup()
+        {
+        }
+
+        [Test]
+        public void CanCreateDatabase()
+        {
+            // ARRANGE
+            _context = EfProvider.GetContext();
+
+            // ACT
+            _context.Database.Initialize(true);
+
+            // ASSERT
+            Assert.NotNull(_context.Database.Connection);
+        }
     }
 }
