1d0c98761a5d845ba92ee4b6ada33dc152e9ff94
Walter Pinson <walter@walterpinson.com>
2013-06-20 11:57:32 -0400

    Add a custom DbContext to handle mappings.

---
+++ src/Infrastructure.Data.SqlAzure/PocFunDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Domain.Models;
+
+namespace Infrastructure.Data.SqlAzure
+{
+    public class PocFunDbContext : DbContext
+    {
+        public PocFunDbContext()
+            : base(string.Format("name={0}", "DefaultConnectionString")) { }
+
+        public DbSet<Job> Jobs { get; set; }
+        public DbSet<JobApplicant> JobApplicants { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.ComplexType<Name>();
+            modelBuilder.ComplexType<Address>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
