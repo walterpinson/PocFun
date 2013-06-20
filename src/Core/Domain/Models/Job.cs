80004320879ff8bc1a477a94dcd436e63a46c63b
Walter Pinson <walter@walterpinson.com>
2013-06-20 10:14:54 -0400

    Changed Id to Guids.  Will use Guid IDs across all database platforms for the sake of PoC.  Previously misunderstood a SO article explaining MongoDb Ids.

--- src/Core/Domain/Models/Job.cs
+++ src/Core/Domain/Models/Job.cs
@@ -4,8 +4,7 @@ namespace Core.Domain.Models
 {
     public class Job
     {
-        public long Id { get; set; }
-        public string IdBig { get; set; }
+        public Guid Id { get; set; }
         public string Title { get; set; }
         public decimal Salary { get; set; }
         public bool IsFilled { get; set; }
--- src/Core/Domain/Models/JobApplicant.cs
+++ src/Core/Domain/Models/JobApplicant.cs
@@ -4,9 +4,9 @@ namespace Core.Domain.Models
 {
     public class JobApplicant
     {
-        public long Id { get; set; }
-        public string IdBig { get; set; }
+        public Guid Id { get; set; }
         public Name Name { get; set; }
         public Address Address { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
