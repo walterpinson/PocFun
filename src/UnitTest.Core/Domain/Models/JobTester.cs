26be1f24c141a797daaccaec9c6eac1b65df02e6
Walter Pinson <walter@walterpinson.com>
2013-06-20 11:16:30 -0400

    Add test harness for Job type.

---
+++ src/UnitTest.Core/Domain/Models/JobTester.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Core.Domain.Models;
+
+namespace UnitTest.Core.Domain.Models
+{
+    [TestFixture]
+    public class JobTester
+    {
+        [Test]
+        public void CanConstructJobWithDefaultConstructor() {
+            // ARRANGE
+
+            // ACT
+            var job = new Job();
+
+            // ASSERT
+            Assert.That(job, Is.Not.Null);
+            Assert.That(job.IsFilled, Is.False);
+        }
+    }
+}
