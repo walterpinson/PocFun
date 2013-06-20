21318bd997192f4c4b4288f5d27f74cad0d446f5
Walter Pinson <walter@alekto.co>
2013-06-20 07:50:16 -0400

    Solution structure set up to support plugn-n-play of multiple database platforms.

---
+++ src/Core/Domain/Models/JobApplicant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Core.Domain.Models
+{
+    public class JobApplicant
+    {
+        public long Id { get; set; }
+        public Guid IdBig { get; set; }
+        public Name Name { get; set; }
+        public Address Address { get; set; }
+    }
+}
