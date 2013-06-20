57cd63ce1c6138447831eae9425eeabd99cd6bd9
Walter Pinson <walter@walterpinson.com>
2013-06-20 16:53:14 -0400

    Begin integrating MongoDb.  Add MongoDb C# driver and MongoRepository NuGet packages.

---
+++ src/Infrastructure.Data.MongoDb/JobApplicantRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Domain.Models;
+using Core.Domain.Services;
+using Infrastructure.Data.MongoDb.Models;
+using MongoRepository;
+
+namespace Infrastructure.Data.MongoDb
+{
+    public class JobApplicantRepository : MongoRepository<MongoJobApplicant>, IJobApplicantRepository
+    {
+        /*
+         * This class will make extensive use of AutoMapper.  MongoRepository requires models to inherit from
+         * IEntity; which decorates the Id field with attributes.  We must not pollute our domain model, so 
+         * we will map between our Core.Models and MangoDb.Models.
+         */
+
+        public JobApplicant Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public JobApplicant Get(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<JobApplicant> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public JobApplicant Create(JobApplicant entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public JobApplicant Update(JobApplicant entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(JobApplicant entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Infrastructure.Data.MongoDb/Models/MongoJobApplicant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoRepository;
+using MongoDB.Bson.Serialization.Attributes;
+using Core.Domain.Models;
+
+namespace Infrastructure.Data.MongoDb.Models
+{
+    public class MongoJobApplicant : IJobApplicant, IEntity
+    {
+        [BsonId]
+        public string Id { get; set; }
+        public Name Name { get; set; }
+        public Address Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
