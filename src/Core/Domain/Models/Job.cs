d3b77cb9697c1693f0c723acb744d5e6f320f23a
Walter Pinson <walter@walterpinson.com>
2013-06-20 10:35:16 -0400

    Implement Fill behavior.

--- src/Core/Domain/Models/Job.cs
+++ src/Core/Domain/Models/Job.cs
@@ -8,10 +8,14 @@ namespace Core.Domain.Models
         public string Title { get; set; }
         public decimal Salary { get; set; }
         public bool IsFilled { get; set; }
-
+        public Guid PersonHiredId { get; set; }
+        public JobApplicant PersonHired { get; set; }
         public virtual IList<JobApplicant> Applicants {get;set;}
-        
 
-        public void Fill(
+
+        public void Fill(JobApplicant personHired)
+        {
+            PersonHired = personHired;
+        }
     }
 }
