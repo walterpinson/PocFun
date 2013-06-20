ca1eae94e10036a3079f84098c9a83aae3f4f542
Walter Pinson <walter@walterpinson.com>
2013-06-20 12:04:42 -0400

    Add a static mechanism that allows the retrieval of a new instance of the custom DbContext.

---
+++ src/Infrastructure.Data.SqlAzure/EfProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.SqlAzure
+{
+    public class EfProvider
+    {
+        public static PocFunDbContext GetContext()
+        {
+            return new PocFunDbContext();
+        }
+    }
+}
