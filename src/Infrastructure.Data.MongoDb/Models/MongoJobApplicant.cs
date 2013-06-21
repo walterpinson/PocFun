77bf963c5540575283b6d37c4e5bcc9d2cab76d4
Walter Pinson <walter@alekto.co>
2013-06-21 11:42:28 -0400

    Remove unnecessary namespace dependencies.

--- src/Infrastructure.Data.MongoDb/Models/MongoJobApplicant.cs
+++ src/Infrastructure.Data.MongoDb/Models/MongoJobApplicant.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MongoRepository;
+﻿using MongoRepository;
 using MongoDB.Bson.Serialization.Attributes;
 using Core.Domain.Models;
 
