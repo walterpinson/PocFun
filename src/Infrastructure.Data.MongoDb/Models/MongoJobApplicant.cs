55996d326eb616eeb91abd6346f631256c9bae11
Walter Pinson <walter@walterpinson.com>
2013-06-24 10:28:27 -0400

    Taking the approach of creating a completely seperate ID for Mongo.  Have to figure out how to normalize this with a non-string GUID that is used for SQL and other repositories.

--- src/Infrastructure.Data.MongoDb/Models/MongoJobApplicant.cs
+++ src/Infrastructure.Data.MongoDb/Models/MongoJobApplicant.cs
@@ -1,15 +1,13 @@
-﻿using MongoRepository;
+﻿using System;
+using MongoRepository;
 using MongoDB.Bson.Serialization.Attributes;
 using Core.Domain.Models;
 
 namespace Infrastructure.Data.MongoDb.Models
 {
-    public class MongoJobApplicant : IJobApplicant, IEntity
+    public class MongoJobApplicant : JobApplicant, IEntity
     {
         [BsonId]
-        public string Id { get; set; }
-        public Name Name { get; set; }
-        public Address Address { get; set; }
-        public string PhoneNumber { get; set; }
+        public string MongoId { get; set; }
     }
 }
