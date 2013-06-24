a97091f9d828bcf8b4eebb1ffc7da680d82b8eba
Walter Pinson <walter@walterpinson.com>
2013-06-24 10:29:55 -0400

    Implement JobRespository and create a test harness for it.

---
+++ src/Infrastructure.Data.SqlAzure/JobRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using Core.Domain.Models;
+using Core.Domain.Services;
+
+namespace Infrastructure.Data.SqlAzure
+{
+    public class JobRepository : EfRepositoryBase<PocFunDbContext>, IJobRepository
+    {
+        #region Implementation of IRepository<Job>
+
+        public Job Get(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Job Get(Guid id)
+        {
+            return Get<Job>(j => j.Id == id);
+        }
+
+        public IQueryable<Job> GetAll()
+        {
+            return GetList<Job>();
+        }
+
+        public Job Create(Job entity)
+        {
+            var operation = Add(entity);
+            Job newJob = null;
+
+            if (operation.Status)
+                newJob = entity;
+
+            return newJob;
+        }
+
+        public Job Update(Job entity)
+        {
+            var operation = Update<Job>(entity);
+            Job updatedJob = null;
+
+            if (operation.Status)
+                updatedJob = entity;
+
+            return updatedJob;
+        }
+
+        public void Delete(Job entity)
+        {
+            Delete<Job>(entity);
+        }
+
+        #endregion
+    }
+}
---
+++ src/UnitTest.Infrastructure.Data.SqlAzure/JobRepositoryTester.cs
@@ -0,0 +1,55 @@
+﻿using Core.Domain.Models;
+using Core.Domain.Services;
+using Infrastructure.Data.SqlAzure;
+using NUnit.Framework;
+
+namespace UnitTest.Infrastructure.Data.SqlAzure
+{
+    [TestFixture]
+    public class JobRepositoryTester
+    {
+        private Job _job;
+        private IJobRepository _subjectUnderTest;
+
+        [SetUp]
+        public void Setup()
+        {
+            _subjectUnderTest = new JobRepository();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _job = null;
+            _subjectUnderTest = null;
+        }
+
+        [Test]
+        public void CanAddJob()
+        {
+            // ARRANGE
+            _job = ConstructJob();
+
+            // ACT
+            var added = _subjectUnderTest.Create(_job);
+
+            // ASSERT
+            Assert.That(added, Is.Not.Null);
+            Assert.That(added.Id, Is.Not.Null);
+        }
+
+        private Job ConstructJob()
+        {
+            var job = new Job
+                {
+                    Title = "Chief Happy Officer",
+                    IsFilled = false,
+                    Salary = 395000m,
+                    Applicants = null,
+                    PersonHired = null
+                };
+
+            return job;
+        }
+    }
+}
