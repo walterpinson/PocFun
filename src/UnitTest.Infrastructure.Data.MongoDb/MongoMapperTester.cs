d5d5e7ca6f9cbb61710893a0751c8f9146ec459f
Walter Pinson <walter@walterpinson.com>
2013-06-25 14:02:06 -0400

    Create a test harness for MongoMapper.  Add AutoMapper to the project.

---
+++ src/UnitTest.Infrastructure.Data.MongoDb/MongoMapperTester.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Data.MongoDb;
+using AutoMapper;
+using NUnit.Framework;
+
+namespace UnitTest.Infrastructure.Data.MongoDb
+{
+    [TestFixture]
+    public class MongoMapperTester
+    {
+        [Test]
+        public void CanConfigureAutoMapperforMongo()
+        {
+            MongoMapper.Configure();
+            Mapper.AssertConfigurationIsValid();
+        }
+    }
+}
