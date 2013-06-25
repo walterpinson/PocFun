b1d1ce69f4cc9bba4dffe35b5ce6d06ae0ad63fd
Walter Pinson <walter@walterpinson.com>
2013-06-25 12:02:03 -0400

    Cleanup type per Resharper.  No longer necessary to overwrite the ID field.  The inherritance takes care of everything for us.

--- src/Infrastructure.Data.MongoDb/Models/MongoJob.cs
+++ src/Infrastructure.Data.MongoDb/Models/MongoJob.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using Core.Domain.Models;
+﻿using Core.Domain.Models;
 using MongoRepository;
-using MongoDB.Bson.Serialization.Attributes;
 
 namespace Infrastructure.Data.MongoDb.Models
 {
-    public class MongoJob : Job, IEntity
-    {
-        [BsonId]
-        //public Guid Id { get; set; }
-        public new Guid Id
-        {
-            get { return base.Id; }
-            set { base.Id = value; }
-        }
-    }
+    public class MongoJob : Job, IEntity {}
 }
