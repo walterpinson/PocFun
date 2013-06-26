0bee905fdd0d098e20469b68f172581ad386ee6f
Walter Pinson <walter@walterpinson.com>
2013-06-26 08:15:34 -0400

    Cleanup unused namesapces.  Implement the basic crud unit tests.

--- src/UnitTest.Infrastructure.Data.MongoDb/JobApplicantRepositoryTester.cs
+++ src/UnitTest.Infrastructure.Data.MongoDb/JobApplicantRepositoryTester.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Core.Domain.Models;
 using Core.Domain.Services;
 using Infrastructure.Data.MongoDb;
@@ -40,6 +37,93 @@ namespace UnitTest.Infrastructure.Data.MongoDb
             DropDb();
             _jobApplicant = null;
             _subjectUnderTest = null;
+        }
+
+        [Test]
+        public void CanAddJobApplicant()
+        {
+            // ARRANGE
+            _jobApplicant = ConstructJobApplicant();
+
+            // ACT
+            var added = _subjectUnderTest.Create(_jobApplicant);
+
+            // ASSERT
+            Assert.That(added, Is.Not.Null);
+            Assert.That(added.Id, Is.Not.Null);
+            Assert.That(added.Id, Is.Not.EqualTo(Guid.Empty));
+        }
+
+        [Test]
+        public void CanGetJobApplicant()
+        {
+            // ARRANGE
+            _jobApplicant = ConstructJobApplicant();
+            var created = _subjectUnderTest.Create(_jobApplicant);
+
+            // ACT
+            var retrieved = _subjectUnderTest.Get(created.Id);
+
+            // ASSERT
+            Assert.That(retrieved, Is.Not.Null);
+            Assert.That(retrieved.Id, Is.EqualTo(created.Id));
+        }
+
+        [Test]
+        public void CanGetAllJobApplicants()
+        {
+            // ARRANGE
+            _jobApplicant = ConstructJobApplicant();
+            var job2 = ConstructJobApplicant();
+            var created1 = _subjectUnderTest.Create(_jobApplicant);
+            var created2 = _subjectUnderTest.Create(job2);
+
+
+            // ACT
+            IQueryable<JobApplicant> retrieved = _subjectUnderTest.GetAll();
+            var retrieved1 = retrieved.Where(j => j.Id == created1.Id);
+            var retrieved2 = retrieved.Where(j => j.Id == created2.Id);
+
+            // ASSERT
+            Assert.That(retrieved, Is.Not.Null);
+            Assert.That(retrieved1, Is.Not.Null);
+            Assert.That(retrieved2, Is.Not.Null);
+        }
+
+        [Test]
+        public void CanUpdateJobApplicant()
+        {
+            // ARRANGE
+            _jobApplicant = ConstructJobApplicant();
+            var created = _subjectUnderTest.Create(_jobApplicant);
+            var retrieved = _subjectUnderTest.Get(created.Id);
+            var newName = new Name { First = "NEW", Last = "NewLast" };
+            retrieved.Name = newName;
+
+
+            // ACT
+            var updated = _subjectUnderTest.Update(retrieved);
+
+            // ASSERT
+            Assert.That(updated, Is.Not.Null);
+            Assert.That(updated.Id, Is.EqualTo(created.Id));
+            Assert.That(updated.Name, Is.EqualTo(newName));
+        }
+
+        [Test]
+        public void CanDeleteJobApplicant()
+        {
+            // ARRANGE
+            _jobApplicant = ConstructJobApplicant();
+            var created = _subjectUnderTest.Create(_jobApplicant);
+
+            // ACT
+            _subjectUnderTest.Delete(created);
+            var retrieved = _subjectUnderTest.Get(created.Id);
+
+            // ASSERT
+            Assert.That(created.Id, Is.Not.EqualTo(Guid.Empty));
+            Assert.That(retrieved, Is.Null);
         }
 
         private void DropDb()
