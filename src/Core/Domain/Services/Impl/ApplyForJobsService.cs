eaef35df332fbaf385cfe446e53fc1be86b3eb22
Walter Pinson <walter@walterpinson.com>
2013-06-26 13:47:13 -0400

    Add implementation for IApplyForJobsService.

---
+++ src/Core/Domain/Services/Impl/ApplyForJobsService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Domain.Models;
+using Core.Domain.Services;
+
+namespace Core.Domain.Services.Impl
+{
+    public class ApplyForJobsService : IApplyForJobsService
+    {
+        public void SubmitApplication(JobApplicant applicant, Job job)
+        {
+            if (null == applicant || null == job)
+                throw new Exception();
+
+            var application = new JobApplication
+            {
+                Applicant = applicant,
+                Date = DateTime.Now
+            };
+
+            job.AcceptApplication(application);
+        }
+    }
+}
