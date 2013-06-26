b76c0d491e75adec42161056a70a1ac936290bc0
Walter Pinson <walter@walterpinson.com>
2013-06-26 10:54:15 -0400

    Implement tests for all CRUD methods.

--- src/UnitTest.Infrastructure.Data.SqlAzure/JobRepositoryTester.cs
+++ src/UnitTest.Infrastructure.Data.SqlAzure/JobRepositoryTester.cs
@@ -1,4 +1,6 @@
-﻿using Core.Domain.Models;
+﻿using System;
+using System.Linq;
+using Core.Domain.Models;
 using Core.Domain.Services;
 using Infrastructure.Data.SqlAzure;
 using NUnit.Framework;
@@ -36,6 +38,80 @@ namespace UnitTest.Infrastructure.Data.SqlAzure
             // ASSERT
             Assert.That(added, Is.Not.Null);
             Assert.That(added.Id, Is.Not.Null);
+            Assert.That(added.Id, Is.Not.EqualTo(Guid.Empty));
+        }
+
+
+        [Test]
+        public void CanGetJob()
+        {
+            // ARRANGE
+            _job = ConstructJob();
+            var created = _subjectUnderTest.Create(_job);
+
+            // ACT
+            var retrieved = _subjectUnderTest.Get(created.Id);
+
+            // ASSERT
+            Assert.That(retrieved, Is.Not.Null);
+            Assert.That(retrieved.Id, Is.EqualTo(created.Id));
+        }
+
+        [Test]
+        public void CanGetAllJobs()
+        {
+            // ARRANGE
+            _job = ConstructJob();
+            var job2 = ConstructJob();
+            var created1 = _subjectUnderTest.Create(_job);
+            var created2 = _subjectUnderTest.Create(job2);
+
+
+            // ACT
+            IQueryable<Job> retrieved = _subjectUnderTest.GetAll();
+            var retrieved1 = retrieved.Where(j => j.Id == created1.Id);
+            var retrieved2 = retrieved.Where(j => j.Id == created2.Id);
+
+            // ASSERT
+            Assert.That(retrieved, Is.Not.Null);
+            Assert.That(retrieved1, Is.Not.Null);
+            Assert.That(retrieved2, Is.Not.Null);
+        }
+
+        [Test]
+        public void CanUpdateJob()
+        {
+            // ARRANGE
+            _job = ConstructJob();
+            var created = _subjectUnderTest.Create(_job);
+            var retrieved = _subjectUnderTest.Get(created.Id);
+            var newTitle = "Better BIGGER New Title";
+            retrieved.Title = newTitle;
+
+
+            // ACT
+            var updated = _subjectUnderTest.Update(retrieved);
+
+            // ASSERT
+            Assert.That(updated, Is.Not.Null);
+            Assert.That(updated.Id, Is.EqualTo(created.Id));
+            Assert.That(updated.Title, Is.EqualTo(newTitle));
+        }
+
+        [Test]
+        public void CanDeleteJob()
+        {
+            // ARRANGE
+            _job = ConstructJob();
+            var created = _subjectUnderTest.Create(_job);
+
+            // ACT
+            _subjectUnderTest.Delete(created);
+            var retrieved = _subjectUnderTest.Get(created.Id);
+
+            // ASSERT
+            Assert.That(created.Id, Is.Not.EqualTo(Guid.Empty));
+            Assert.That(retrieved, Is.Null);
         }
 
         private Job ConstructJob()
