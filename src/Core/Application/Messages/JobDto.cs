41bb77bb127d347c58ac1a5d3a51c54e08eb6916
Walter Pinson <walter@walterpinson.com>
2013-06-26 13:05:43 -0400

    Define a Recruiter service and the DTO types that it returns to clients/consumers.

---
+++ src/Core/Application/Messages/JobDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Messages
+{
+    public class JobDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public decimal Salary { get; set; }
+        public bool IsFilled { get; set; }
+        public Guid? PersonHiredId { get; set; }
+    }
+}
---
+++ src/Core/Application/Services/IRecruiter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Application.Messages;
+
+namespace Core.Application.Services
+{
+    public interface IRecruiter
+    {
+        JobDto GetJob(Guid jobId);
+        IList<JobDto> GetJobs();
+        IList<JobApplicantDto> GetApplicants();
+        IList<JobApplicantDto> GetApplicants(Guid jobId);
+
+        void Apply(JobDto job, JobApplicantDto applicant);
+        void Hire(JobDto job, JobApplicantDto applicant);
+    }
+}
