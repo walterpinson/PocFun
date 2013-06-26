fa0b194daa347922b568d11545cd4c78e1f2ea85
Walter Pinson <walter@walterpinson.com>
2013-06-26 15:58:45 -0400

    Create an ITokenService interface and frame out the implementation.

---
+++ src/Infrastructure.Server/TokenService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Core.Application.Services;
+
+namespace Infrastructure.Server
+{
+    public class TokenService : ITokenService
+    {
+        public string Generate(string userId, string ipAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///  We won't pass any date/time to the validation function.  Instead we will use a
+        ///  configurable TTL.  The validation will do a diff between the time embedded in the
+        ///  token and now.  If the diff is greater than the TTL, the the token will be invalid.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="userId"></param>
+        /// <param name="ipAddress"></param>
+        /// <returns></returns>
+        public bool Validate(string token, string userId, string ipAddress)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
