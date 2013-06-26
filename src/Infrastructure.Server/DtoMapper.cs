fd423088272388af495bfb88f5b60b198e902b96
Walter Pinson <walter@walterpinson.com>
2013-06-26 13:12:03 -0400

    Add server implementation of IRecuiter service and include a Mapper for the DTO types.

---
+++ src/Infrastructure.Server/DtoMapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Domain.Models;
+using Core.Application.Messages;
+using AutoMapper;
+
+namespace Infrastructure.Server
+{
+    public static class DtoMapper
+    {
+        public static void Configure()
+        {
+            Mapper.CreateMap<Job, JobDto>();
+            Mapper.CreateMap<JobDto, Job>();
+
+            Mapper.CreateMap<JobApplicant, JobApplicantDto>();
+            Mapper.CreateMap<JobApplicantDto, JobApplicant>();
+        }
+    }
+}
---
+++ src/Infrastructure.Server/Recruiter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Domain.Models;
+using Core.Domain.Services;
+using Core.Application.Messages;
+using Core.Application.Services;
+
+namespace Infrastructure.Server
+{
+    public class Recruiter
+    {
+        private IApplyForJobsService _applyForJobsService;
+        private IJobApplicantRepository _jobApplicantRepository;
+        private IJobRepository _jobRepository;
+
+        public Recruiter(IApplyForJobsService applyForJobsService, IJobRepository jobRepo, IJobApplicantRepository jobApplicantRepo)
+        {
+            _applyForJobsService = applyForJobsService;
+            _jobApplicantRepository = jobApplicantRepo;
+            _jobRepository = jobRepo;
+        }
+    }
+}
