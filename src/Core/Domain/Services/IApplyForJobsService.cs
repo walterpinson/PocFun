c3ccb99fe361e532dab05c65403535a29353a567
Walter Pinson <walter@walterpinson.com>
2013-06-26 13:10:04 -0400

    Define a domain service responsible for creating applications for job applicants.

---
+++ src/Core/Domain/Services/IApplyForJobsService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Domain.Models;
+
+namespace Core.Domain.Services
+{
+    public interface IApplyForJobsService
+    {
+        void SubmitApplication(JobApplicant applicant, Job job);
+    }
+}
