24aa7acd556d17f3a6211591c870707f6f85ef94
Walter Pinson <walter@walterpinson.com>
2013-07-03 13:19:46 -0400

    Cleanup per Resharper.  Make all properties public.

--- src/Core/Application/Messages/JobApplicantDto.cs
+++ src/Core/Application/Messages/JobApplicantDto.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Core.Domain.Models;
 
 namespace Core.Application.Messages
 {
     public class JobApplicantDto
     {
-        Guid Id { get; set; }
-        Address Address { get; set; }
-        Name Name { get; set; }
-        string PhoneNumber { get; set; }
+        public Guid Id { get; set; }
+        public Address Address { get; set; }
+        public Name Name { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
