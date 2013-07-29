68bdb2f689457b86a680efdcac0b4505de077498
Walter Pinson <walter@walterpinson.com>
2013-07-29 07:17:43 -0400

    Begin implememtation of TokenService.

--- src/Infrastructure.Security/TokenService.cs
+++ src/Infrastructure.Security/TokenService.cs
@@ -6,30 +6,33 @@ namespace Infrastructure.Security
 {
     public class TokenService : ITokenService
     {
-        private byte[] _secretKeyHmac;
+        private static byte[] _secretKeyHmac;
         private byte[] _secretKeyEncryption;
 
         #region Implementation of ITokenProvider
 
-        public string GenerateToken(string userId, string ipAddress)
+        public string GenerateToken(string userId, string ipAddress, DateTime issueDate)
         {
-            _secretKeyHmac = new byte[64];
-            using(var rng = new RNGCryptoServiceProvider())
-            {
-                // Fill the array with cryptographically strong random bytes
-                rng.GetBytes(_secretKeyHmac);
-            }
-
             var token = new Token();
 
             return token.Tokenize();
         }
 
-        public bool ValidateToken(string token, string userId, string ipAddress)
+        public bool ValidateToken(string token, string userId, string ipAddress, DateTime issueDate)
         {
             return true;
         }
 
         #endregion
+
+        public static void Initialize()
+        {
+            _secretKeyHmac = new byte[64];
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                // Fill the array with cryptographically strong random bytes
+                rng.GetBytes(_secretKeyHmac);
+            }
+        }
     }
 }
