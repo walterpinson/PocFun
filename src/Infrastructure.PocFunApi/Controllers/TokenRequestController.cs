800542694af218be17f2123b203c507e5331c285
Walter Pinson <walter@walterpinson.com>
2013-07-29 07:22:16 -0400

    Implement separate controllers for TokenRequest and for TokenValidation.

---
+++ src/Infrastructure.PocFunApi/Controllers/TokenRequestController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Core.Application.Services;
+using Infrastructure.PocFunApi.Models;
+
+namespace Infrastructure.PocFunApi.Controllers
+{
+    public class TokenRequestController : ApiController
+    {
+        private readonly ITokenService _tokenService ;
+
+        public TokenRequestController(ITokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        // GET api/tokenrequest
+        public IEnumerable<string> Get()
+        {
+            return new[] { "value1", "value2" };
+        }
+
+        // POST api/tokenrequest
+        public string Post(TokenRequest request)
+        {
+            return _tokenService.GenerateToken(request.UserName, request.IpAddress, request.RequestDate);
+        }
+    }
+}
