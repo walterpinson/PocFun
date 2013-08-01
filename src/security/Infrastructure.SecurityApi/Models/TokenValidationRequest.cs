e97e06d73e396c49645e937c3967d051804813c3
Walter Pinson <walter@walterpinson.com>
2013-08-01 16:03:47 -0400

    We only need to send the token in the request.  We aren't trusing any other data from the client.

--- src/security/Infrastructure.SecurityApi/Models/TokenValidationRequest.cs
+++ src/security/Infrastructure.SecurityApi/Models/TokenValidationRequest.cs
@@ -5,8 +5,5 @@ namespace Infrastructure.SecurityApi.Models
     public class TokenValidationRequest
     {
         public string Token { get; set; }
-        public string UserName { get; set; }
-        public string IpAddress { get; set; }
-        public DateTime RequestDate { get; set; }
     }
 }
