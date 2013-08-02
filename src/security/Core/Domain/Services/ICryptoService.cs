f4bb86fb4e7ef32ce563d5c5e4a2621cbca1eae1
Walter Pinson <walter@walterpinson.com>
2013-08-02 15:44:14 -0400

    Add interface methods with strings in and out.

--- src/security/Core/Domain/Services/ICryptoService.cs
+++ src/security/Core/Domain/Services/ICryptoService.cs
@@ -4,7 +4,10 @@ namespace Core.Domain.Services
 {
     public interface ICryptoService
     {
-        byte[] Encrypt(byte[] buffer);
-        byte[] Decrypt(byte[] cipher);
+//        byte[] Encrypt(byte[] buffer);
+//        byte[] Decrypt(byte[] cipher);
+
+        string Encrypt(string buffer);
+        string Decrypt(string cipher);
     }
 }
