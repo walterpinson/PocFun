90be191ab973028574c7802137a7e048c8c2191e
Walter Pinson <walter@walterpinson.com>
2013-08-04 15:14:18 -0400

    Add custom JSON serialization contract resolver to handle types with private setters.

---
+++ src/security/Infrastructure.Server/JsonPrivateSetterContractResolver.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Infrastructure.Server
+{
+    public class JsonPrivateSetterContractResolver : DefaultContractResolver
+    {
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            var propertyOrField = base.CreateProperty(member, memberSerialization);
+
+            if (!propertyOrField.Writable)
+            {
+                var property = member as PropertyInfo;
+                if (property != null)
+                {
+                    var hasPrivateSetter = property.GetSetMethod(true) != null;
+                    propertyOrField.Writable = hasPrivateSetter;
+                }
+            }
+
+            return propertyOrField;
+        }
+    }
+}
