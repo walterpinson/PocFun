a4b4d837b0fd7e0764b9c89e94f228a5bf9782a5
Walter Pinson <walter@walterpinson.com>
2013-08-04 15:10:35 -0400

    Add stubbed out CryptoService implementation.  Each method merely returns the string that it was passed untouched.

---
+++ src/security/Infrastructure.Cryptography/CryptoService.cs
@@ -0,0 +1,21 @@
+﻿using Core.Domain.Services;
+
+namespace Infrastructure.Cryptography
+{
+    public class CryptoService : ICryptoService
+    {
+        #region Implementation of ICryptoService
+
+        public string Encrypt(string buffer)
+        {
+            return buffer;
+        }
+
+        public string Decrypt(string cipher)
+        {
+            return cipher;
+        }
+
+        #endregion
+    }
+}
